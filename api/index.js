2ed1b56b19de2efa0d35a20c7cb971d760a14643
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-13 14:08:28 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
