b2e349cb894dd5f44c9112375c7edeacbc6ec1fa
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-14 01:15:45 +0600

    complete sign in page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,15 +1,19 @@
 const User = require("../models/user.model.js");
 const bcryptjs = require("bcryptjs");
-const errorHanlder = require("../utils/error.js");
+const errorHandler = require("../utils/error.js");
 const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
+
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
+
   try {
+
     await newUser.save();
     res.status(201).json("User created successfully");
+
   } catch (error) {
     //  res.status(500).json(error.message);
     next(error);
@@ -18,13 +22,13 @@ const signup = async (req, res, next) => {
 
 const signin = async (req, res, next) => {
   const { email, password } = req.body;
-
+  
   try {
-    const validUser = await User.findOne({ email: email });
-    if (!validUser) return next(errorHanlder(404, "User not found!"));
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
 
     const validpassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validpassword) return next(errorHanlder(401, "Wrong credentials !"));
+    if (!validpassword) return next(errorHandler(401, "Wrong credentials !"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const {password:pass,...rest} = validUser._doc;
@@ -37,4 +41,4 @@ const signin = async (req, res, next) => {
   }
 };
 
-module.exports = { signup, signin };
+module.exports = {signin,signup};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {signup,signin} = require('../controller/auth.controller.js');
+const {signin,signup} = require('../controller/auth.controller.js');
 const router = express.Router();
 
 router.post('/signup',signup);
