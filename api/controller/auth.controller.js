e57bd03ccf13172f5e05d67bc16d6ace1fe026f0
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-13 19:17:31 +0600

    create a middleware & a function to possible error

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 const User  = require('../models/user.model.js');
 const bcryptjs = require('bcryptjs');
+const errorHanlder = require('../utils/error.js');
 
-const signup = async (req,res)=>{
+const signup = async (req,res,next)=>{
 
   const {username,email,password} = req.body;
   const hashedPassword = bcryptjs.hashSync(password,10);
@@ -10,9 +11,9 @@ const signup = async (req,res)=>{
     await newUser.save();
     res.status(201).json("User created successfully");
   }catch(error){
-    res.status(500).json(error.message);
+  //  res.status(500).json(error.message);
+      next(error);
   }
-  
 }
 
 module.exports = signup;
---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+const errorHanlder = (statusCode,message)=>{
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+}
+module.exports = errorHanlder;
