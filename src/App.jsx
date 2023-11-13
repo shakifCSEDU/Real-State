bc99b464374b2fe13e221ab809c3e65b31b65f4d
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-13 13:17:32 +0600

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SigIn from "./pages/SigIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SigIn />} />
