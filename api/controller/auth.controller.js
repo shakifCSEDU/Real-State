e77d369c3cf9dd45e742e699452121519fba5703
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-13 21:02:17 +0600

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,19 +1,40 @@
-const User  = require('../models/user.model.js');
-const bcryptjs = require('bcryptjs');
-const errorHanlder = require('../utils/error.js');
+const User = require("../models/user.model.js");
+const bcryptjs = require("bcryptjs");
+const errorHanlder = require("../utils/error.js");
+const jwt = require("jsonwebtoken");
 
-const signup = async (req,res,next)=>{
-
-  const {username,email,password} = req.body;
-  const hashedPassword = bcryptjs.hashSync(password,10);
-  const newUser = new User({username,email,password:hashedPassword});
-  try{
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
     res.status(201).json("User created successfully");
-  }catch(error){
-  //  res.status(500).json(error.message);
-      next(error);
+  } catch (error) {
+    //  res.status(500).json(error.message);
+    next(error);
   }
-}
+};
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHanlder(404, "User not found!"));
+
+    const validpassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validpassword) return next(errorHanlder(401, "Wrong credentials !"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password:pass,...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
