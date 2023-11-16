cd70d8cff65b739216d5b9ddc5692f0f4b14a555
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-11-16 22:17:13 +0600

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { app } from "./../firebase";
+import { Link } from "react-router-dom";
 import {
   getDownloadURL,
   getStorage,
@@ -193,6 +194,12 @@ const Profile = () => {
         >
           {loading ? "Loading" : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to="/create-listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5 font-bold">
         <span
