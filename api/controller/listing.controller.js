7800e589a0353575c950b817658a608e9a91a548
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-12-07 12:43:11 +0600

    create update listing api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -28,4 +28,26 @@ const deleteListing = async (req, res, next) => {
   }
 };
 
-module.exports = {createListing,deleteListing};
+const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHanlder(404, "Listing not found!"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHanlder(401, "You can only update your own listings!"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing)
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing, updateListing };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {createListing,deleteListing} = require('../controller/listing.controller.js');
+const {createListing,deleteListing,updateListing} = require('../controller/listing.controller.js');
 const { verifyToken } = require('../utils/verifyUser');
 
 router.post("/create",verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
+router.post('/update/:id',verifyToken,updateListing);
+
 
 module.exports = router;
