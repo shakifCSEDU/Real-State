a89c9934938674c1eb0cc8895b53ea809bb359d1
Md Shakif Sahriar <sakifsahriar@gmail.com>
2023-12-11 12:22:45 +0600

    complete search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {createListing,deleteListing,updateListing,getListing} = require('../controller/listing.controller.js');
-const { verifyToken } = require('../utils/verifyUser');
+const {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  getListings,
+} = require("../controller/listing.controller.js");
+const { verifyToken } = require("../utils/verifyUser");
 
-router.post("/create",verifyToken,createListing);
-router.delete('/delete/:id',verifyToken,deleteListing);
-router.post('/update/:id',verifyToken,updateListing);
-router.get('/get/:id',getListing);
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 module.exports = router;
